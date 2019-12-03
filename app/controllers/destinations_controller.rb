eec57e30daf63a3c298cd0db3f8b10dd0ded9ede
ejgann <aspentree79@gmail.com>
2019-12-03 15:43:01 -0500

    added session resources

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,10 +1,12 @@
 class DestinationsController < ApplicationController
      def create
         @destination = Destination.create(user_id: params[:user_id], location_id: params[:location_id])
-        redirect_to users_path
+        redirect_to destinations_path
+     end
+
+     def index
+      @destination = Destination.all
      end
 
 end
 
-
-git remote add demo_app 'git@github.com:levelone/demo_app.git'
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,5 +2,6 @@ class LocationsController < ApplicationController
 
     def show
         @location = Location.find_by(params[:id])
+        
     end
 end
