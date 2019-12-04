f5ac9c86218ac86daee8db875df9fa9a469516bc
ejgann <aspentree79@gmail.com>
2019-12-04 14:49:21 -0500

    Should have committed sooner -- skateboard done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_212126) do
+ActiveRecord::Schema.define(version: 2019_12_04_143037) do
 
   create_table "destinations", force: :cascade do |t|
     t.integer "user_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_212126) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
   end
 
   create_table "location_items", force: :cascade do |t|
