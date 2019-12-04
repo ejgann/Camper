7e6a6f7d25c5efda3df46b83a2a9373035671ba0
ejgann <aspentree79@gmail.com>
2019-12-04 15:56:13 -0500

    skateboard

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   resources :users
   root 'application#home'
   
+  get '/welcome', to: 'sessions#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: "sessions#logout"
+
 end
