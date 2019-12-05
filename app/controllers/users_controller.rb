b71dd1266e7cd75e8c3d2de2a8ca02aa6a24aa5f
Sebastian Delima <sebastiandelima@Cactus1.local>
2019-12-05 17:30:07 -0500

    password working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,24 @@
 class UsersController < ApplicationController
-    
-    before_action :logged_in?
 
-    def welcome
-        
+    before_action :logged_in?, except: [:new, :create]
+
+    def welcome 
     end
 
     def new
+       if session[:user] != nil
+        redirect_to welcome_path
+       else
         @user = User.new
+        render :new
+       end
     end
 
     def create
         @user = User.new(strong_params)
-        if @user.valid?
+        password = strong_params["password"]
+        confirm_password = strong_params["password_confirmation"]
+        if @user.valid? && password == confirm_password
             @user.save
             session[:user] = @user
             redirect_to welcome_path
@@ -28,6 +34,6 @@ class UsersController < ApplicationController
     private
 
     def strong_params
-        params.require(:user).permit(:name)
+        params.require(:user).permit(:name, :password, :password_confirmation)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_04_143037) do
+ActiveRecord::Schema.define(version: 2019_12_05_195114) do
 
   create_table "destinations", force: :cascade do |t|
     t.integer "user_id"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2019_12_04_143037) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
